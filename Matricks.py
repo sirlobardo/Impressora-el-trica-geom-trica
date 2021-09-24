e03bf2d97aaf5690a5874aeced8e5ec083b50788
Eduardo Lobo Teixeira Filho <lobardo@pop-os.localdomain>
2021-09-24 17:31:53 -0300

    correct mistake

--- Matricks.py
+++ Matricks.py
@@ -3,20 +3,39 @@ import math
 import numpy as np
 import cmath
 
-class Bhaskara:
-    def __init__(self, A, B, C):
-        self.a = float(A)
-        self.b = float(B)
-        self.c = float(C)
+
+
+class Printer:
+
+    def __init__(self, campo, vel_linear, vel_angular):
+       self.e = float(1.6*math.pow(10, -19))
+       self.m = float(9*math.pow(10, -31))
+       self.c = float(campo)
+       self.v = float(vel_linear)
+       self.w = float(vel_angular)
     pass
 
-    def delta(self):
-        return self.b**2 - 4*self.a*self.c
-    
-    def raiz1(self):
-        return (- b + cmath.sqrt(Bhaskara(self.a, self.b, self.c).delta())) / (2*self.a)
+'''    def Plot(self):
+        dif = self.max - self.min
 
-    def raiz2(self):
-        return (- b - cmath.sqrt(Bhaskara(self.a, self.b, self.c).delta())) / (2*self.a)
-        
-        
+        y = np.zeros(dif*100)
+        x = np.linspace(self.min, self.max , dif*100)
+
+        for i in range(0, dif*100):
+
+    # HERE YOU PUT THE FORMULA#
+    #x[i] is the x of the math function#
+
+            y[i] = 50*x[i]
+        return plt.plot(x,y)
+
+    def Config(self):
+        plt.title(self.title)
+        plt.xlabel(self.xlabel)
+        plt.ylabel(self.ylabel)
+
+    def Show(self):
+        Graph(self.min, self.max, self.title, self.xlabel, self.ylabel).Plot()
+        Graph(self.min, self.max,  self.title, self.xlabel, self.ylabel).Config()
+        plt.show()
+'''
