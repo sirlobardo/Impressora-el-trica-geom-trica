e80e3a283e7ddbff3584d38d86f7b399c846bd9a
Eduardo Lobo Teixeira Filho <lobardo@pop-os.localdomain>
2021-10-02 18:09:47 -0300

    adjust line 7 of main

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ campo = input("Campo: ")
 vel = input("Velocidade Linear: ")
 e = input("Número de Elétrons: ")
 c = input("Comprimento : ")
-Impressora(campo, vel, e, c ).Graph()
+Impressora(campo, vel, e, c).Graph()
 input("Pressione qualquer tecla para continuar ...")
 
